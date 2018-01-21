999ed5af17b933c1c3d35a2a305b7b295a323cb7
Rimex1 <alexdiaz10129@gmail.com>
2018-01-21 00:31:10 -0600

    Variables
    The different kinds of variables and examples of them

---
+++ explanations.js
@@ -0,0 +1,16 @@
+// string
+var favColor = 'red';
+// array
+var myFavColors = ['blue', 'red', 'green'];
+// number
+var numOfFavColors = 3;
+// boolean
+var hasGotFavColors = true;
+// object
+var richObject = {
+	firstName: 'rich',
+	lastName: 'Armsstrong',
+	favColors: ['blue', 'red', 'green'],
+	yearAlive: 103,
+	isMale: true
+}
