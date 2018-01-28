cf75b3032e4cef689af83bd4928a54553fb8bbe8
Rimex1 <alexdiaz10129@gmail.com>
2018-01-28 00:18:32 -0600

    functions and else if statements

--- explanations.js
+++ explanations.js
@@ -14,3 +14,40 @@ var richObject = {
 	yearAlive: 103,
 	isMale: true
 }
+
+// Explanations
+var fullName = function (firstname, 
+	secondname) {
+	return firstname + ' ' +secondname;
+}
+
+var area = function (width, height) {
+	return width * height;
+}
+
+var whatIsMyFavColor = function () {
+	return false;
+
+	return true;
+
+	return 'blue';
+}
+
+var doSomething = function () {
+	console.log("do Something!");
+}
+
+// Explanations
+
+var name1 = 'Alex';
+var name2 = 'D';
+
+if (name1 == 'Alex' && name2 == 'D') {
+	//Dosomething
+	alert('true');
+
+} else if (name2 == 'D'){
+	alert('true!');
+} else {
+	alert(false);
+}
